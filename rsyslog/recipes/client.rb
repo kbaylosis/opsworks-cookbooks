3e2c2175683a2b4de982b1aa745468b76cdcb80d
Doc Ritezel <pair+doc@ministryofvelocity.com>
2013-11-08 00:59:54 -0800

    Add papertrail-rsyslog

--- rsyslog/recipes/client.rb
+++ rsyslog/recipes/client.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: rsyslog
 # Recipe:: client
 #
-# Copyright 2009-2013, Opscode, Inc.
+# Copyright 2009-2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,41 +17,35 @@
 # limitations under the License.
 #
 
-# Do not run this recipe if the server attribute is set
-return if node['rsyslog']['server']
+include_recipe "rsyslog"
 
-include_recipe 'rsyslog::default'
+if !node['rsyslog']['server'] and node['rsyslog']['server_ip'].nil? and Chef::Config[:solo]
+  Chef::Log.fatal("Chef Solo does not support search, therefore it is a requirement of the rsyslog::client recipe that the attribute 'server_ip' is set when using Chef Solo. 'server_ip' is not set.")
+elsif !node['rsyslog']['server']
+  rsyslog_server = node['rsyslog']['server_ip'] ||
+                   search(:node, node['rsyslog']['server_search']).first['ipaddress'] rescue nil
 
-# On Chef Solo, we use the node['rsyslog']['server_ip'] attribute, and on
-# normal Chef, we leverage the search query.
-if Chef::Config[:solo]
-  if node['rsyslog']['server_ip']
-    rsyslog_servers = Array(node['rsyslog']['server_ip'])
-  else
-    Chef::Application.fatal!("Chef Solo does not support search. You must set node['rsyslog']['server_ip']!")
+  if rsyslog_server.nil?
+    raise "The rsyslog::client recipe was unable to determine the remote syslog server. Checked both the server_ip attribute and search()"
   end
-else
-  results = search(:node, node['rsyslog']['server_search']).map { |n| n['ipaddress'] }
-  rsyslog_servers = Array(node['rsyslog']['server_ip']) + Array(results)
-end
 
-if rsyslog_servers.empty?
-  Chef::Application.fatal!('The rsyslog::client recipe was unable to determine the remote syslog server. Checked both the server_ip attribute and search!')
-end
+  template "/etc/rsyslog.d/49-remote.conf" do
+    only_if { node['rsyslog']['remote_logs'] && !rsyslog_server.nil? }
+    source "49-remote.conf.erb"
+    backup false
+    variables(
+      :server => rsyslog_server,
+      :protocol => node['rsyslog']['protocol']
+    )
+    owner node["rsyslog"]["user"]
+    group node["rsyslog"]["group"]
+    mode 0644
+    notifies :restart, "service[#{node['rsyslog']['service_name']}]"
+  end
 
-remote_type = node['rsyslog']['use_relp'] ? 'relp' : 'remote'
-
-template "#{node['rsyslog']['config_prefix']}/rsyslog.d/49-remote.conf" do
-  source    "49-#{remote_type}.conf.erb"
-  owner     'root'
-  group     'root'
-  mode      '0644'
-  variables(:servers => rsyslog_servers)
-  notifies  :restart, "service[#{node['rsyslog']['service_name']}]"
-  only_if   { node['rsyslog']['remote_logs'] }
-end
-
-file "#{node['rsyslog']['config_prefix']}/rsyslog.d/server.conf" do
-  action   :delete
-  notifies :reload, "service[#{node['rsyslog']['service_name']}]"
+  file "/etc/rsyslog.d/server.conf" do
+    only_if do ::File.exists?("/etc/rsyslog.d/server.conf") end
+    action :delete
+    notifies :reload, "service[#{node['rsyslog']['service_name']}]"
+  end
 end
--- rsyslog/recipes/server.rb
+++ rsyslog/recipes/server.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: rsyslog
 # Recipe:: server
 #
-# Copyright 2009-2013, Opscode, Inc.
+# Copyright 2009-2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,28 +17,40 @@
 # limitations under the License.
 #
 
-# Manually set this attribute
+include_recipe "rsyslog"
+
 node.set['rsyslog']['server'] = true
+node.save unless Chef::Config[:solo]
 
-include_recipe 'rsyslog::default'
-
-directory node['rsyslog']['log_dir'] do
-  owner    'root'
-  group    'root'
-  mode     '0755'
+directory ::File.dirname(node['rsyslog']['log_dir']) do
+  owner node["rsyslog"]["user"]
+  group node["rsyslog"]["group"]
   recursive true
+  mode 0755
 end
 
-template "#{node['rsyslog']['config_prefix']}/rsyslog.d/35-server-per-host.conf" do
-  source   '35-server-per-host.conf.erb'
-  owner    'root'
-  group    'root'
-  mode     '0644'
+directory node['rsyslog']['log_dir'] do
+  owner node['rsyslog']['user']
+  group node['rsyslog']['group']
+  mode 0755
+end
+
+template "/etc/rsyslog.d/35-server-per-host.conf" do
+  source "35-server-per-host.conf.erb"
+  backup false
+  variables(
+    :log_dir => node['rsyslog']['log_dir'],
+    :per_host_dir => node['rsyslog']['per_host_dir']
+  )
+  owner node["rsyslog"]["user"]
+  group node["rsyslog"]["group"]
+  mode 0644
   notifies :restart, "service[#{node['rsyslog']['service_name']}]"
 end
 
-file "#{node['rsyslog']['config_prefix']}/rsyslog.d/remote.conf" do
-  action   :delete
+file "/etc/rsyslog.d/remote.conf" do
+  action :delete
+  backup false
   notifies :reload, "service[#{node['rsyslog']['service_name']}]"
-  only_if  { ::File.exists?("#{node['rsyslog']['config_prefix']}/rsyslog.d/remote.conf") }
+  only_if do ::File.exists?("/etc/rsyslog.d/remote.conf") end
 end
